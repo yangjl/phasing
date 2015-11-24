c2e09671d97890f0f4df97767ae6f96119247eb7
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-11-23 17:31:38 -0800

    submit the simulations.

--- slurm-scripts/sim_step1.sh
+++ slurm-scripts/sim_step1.sh
@@ -8,4 +8,4 @@
 #only argument is mean (of poisson) of number of crossovers
 sizearray=$1
 R --no-save "--args $sizearray $SLURM_JOB_ID" < profiling/1.simulation/1.A.1_impute_parent_sim.R
-#sbatch -p serial slurm-scripts/sim_step1.sh 10 10000
+#sbatch -p bigmeml slurm-scripts/sim_step1.sh 10 10000
