b3047ce9cdff807c09a9fb3c0247ab8e4db56c23
yangjl <jolyang@ucdavis.edu>
2015-11-30 17:27:27 -0800

    run array

--- slurm-scripts/step3_aj_ip.sh
+++ slurm-scripts/step3_aj_ip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
-#SBATCH -D /Users/yangjl/Documents/Github/phasing
-#SBATCH -o /Users/yangjl/Documents/Github/phasing/slurm-log/testout-%j.txt
-#SBATCH -e /Users/yangjl/Documents/Github/phasing/slurm-log/err-%j.txt
+#SBATCH -D /home/jolyang/Documents/Github/phasing
+#SBATCH -o /home/jolyang/Documents/Github/phasing/slurm-log/testout-%j.txt
+#SBATCH -e /home/jolyang/Documents/Github/phasing/slurm-log/err-%j.txt
 #SBATCH -J imputeR
-#SBATCH --array=1-10
+#SBATCH --array=1-500
 #SBATCH --mail-user=yangjl0930@gmail.com
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL #email if fails
 set -e
 set -u
 
-R --no-save '--args $SLURM_ARRAY_TASK_ID' < profiling/2.cjdata/2.B.1_code_impute_parent.R
+R --no-save "--args ${SLURM_ARRAY_TASK_ID}" < profiling/2.cjdata/2.B.1_code_impute_parent.R
