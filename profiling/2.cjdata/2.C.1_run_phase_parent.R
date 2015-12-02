5938c99678aeb995ed0a0fc8ded7ef00be084812
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-12-01 18:59:46 -0800

    run pp

--- profiling/2.cjdata/2.C.1_run_phase_parent.R
+++ profiling/2.cjdata/2.C.1_run_phase_parent.R
@@ -1,21 +1,25 @@
 ### Jinliang Yang
 ### Nov 1st, 2015
 
-CHRI <- 10
+##get command line args
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
 
-
+CHRID <- as.numeric(as.character(args[1]))
+JOBID <- as.numeric(as.character(args[2]))
+print(CHRID)
+print(JOBID)
 
 ##############
 library("imputeR")
-files <- list.files(path="largedata/obs", pattern=paste0("_chr", CHRI, ".RData"), full.names=TRUE)
+files <- list.files(path="largedata/obs", pattern=paste0("_chr", CHRID, ".RData"), full.names=TRUE)
 
-ipdat <- read.csv(paste0("largedata/ip/chr", CHRI, "_ip50.csv"))
+ipdat <- read.csv(paste0("largedata/ip/chr", CHRID, "_ip50.csv"))
 nms <- gsub("p.*_PC", "PC", names(ipdat))
 nms <- gsub("\\.", ":", nms)
 names(ipdat) <- nms
 
-o <- load(files[1])
-
+o <- load(files[JOBID])
 
 GBS.array <- update_gbs_parents(GBS.array=obj, ipdat)
 # get probability matrices
@@ -24,6 +28,7 @@ probs <- error_mx(hom.error=0.02, het.error=0.8, imiss=0.5)
 phase <- phase_parent(GBS.array, win_length=10, join_length=10, verbose=TRUE)
 
 # compute error rate
-out <- phase_error_rate(GBS.array, phase)
-
+outfile <- gsub("/obs/", "/pp/", files[JOBID])
+outfile <- gsub("RData", "csv", outfile)
+write.table(phase, outfile, sep=",", row.names=FALSE, quote=FALSE)
 
---
+++ profiling/2.cjdata/2.C.2_setup_code.R
@@ -0,0 +1,16 @@
+### Jinliang Yang
+### use impute_parent in CJ data
+
+#$SLURM_ARRAY_TASK_ID $SLURM_JOB_ID
+source("~/Documents/Github/zmSNPtools/Rcodes/set_arrayjob.R")
+
+# run array job of impute_parents
+set_arrayjob(shid="largedata/scripts/step4_pp.sh",
+             shcode='R --no-save "--args ${SLURM_ARRAY_TASK_ID} 10" < profiling/2.cjdata/2.C.1_run_phase_parent.R',
+             arrayjobs="1-50",
+             wd=NULL, jobid="imputeR-pp", email="yangjl0930@gmail.com")
+
+###>>> In this path: cd /home/jolyang/Documents/Github/phasing
+###>>> [ note: --ntasks=INT, number of cup ]
+###>>> [ note: --mem=16000, 16G memory ]
+###>>> RUN: sbatch -p serial slurm-scripts/step3_aj_ip.sh
