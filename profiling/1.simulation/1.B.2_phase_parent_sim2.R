60c840c041d27653b74f57e3f44fb2f79fe60838
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-12-06 22:14:34 -0800

    more changes

---
+++ profiling/1.simulation/1.B.2_phase_parent_sim2.R
@@ -0,0 +1,31 @@
+### Jinliang Yang
+### Dec. 1st, 2015
+
+##get command line args
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+
+SIZE <- as.numeric(as.character(args[1]))
+
+library(imputeR)
+
+# to make the random events repeatable
+set.seed(1234579)
+# simulate a GBS.array object
+GBS.array <- sim.array(size.array=SIZE, numloci=10000, hom.error = 0.02, het.error = 0.8,
+                       rec = 0.25, selfing = 0, imiss = 0.5, misscode = 3)
+# get perfect parent genotype
+GBS.array <- get_true_GBS(GBS.array)
+# get probability matrices
+probs <- error_mx2(hom.error=0.02, het.error=0.8)
+# phasing   
+phase <- phase_parent(GBS.array, win_length=10, join_length=10, self_cutoff = 100, verbose=TRUE)
+
+# compute error rate
+out <- phase_error_rate(GBS.array, phase)
+
+outfile <- paste0("largedata/sim2/size", SIZE, "_oc_10kloci.csv")
+write.table(out, outfile, sep=",", row.names=FALSE, quote=FALSE)
+
+
+
--- profiling/1.simulation/1.B.3_setup_code.R
+++ profiling/1.simulation/1.B.3_setup_code.R
@@ -14,3 +14,17 @@ set_arrayjob(shid="largedata/scripts/step4_sim_pp_oc.sh",
 ###>>> [ note: --ntasks=INT, number of cup ]
 ###>>> [ note: --mem=16000, 16G memory ]
 ###>>> RUN: sbatch -p serial --mem 8000 largedata/scripts/step4_sim_pp.sh
+
+#$SLURM_ARRAY_TASK_ID $SLURM_JOB_ID
+source("~/Documents/Github/zmSNPtools/Rcodes/set_arrayjob.R")
+
+# run array job of impute_parents
+set_arrayjob(shid="largedata/scripts/step4_sim_pp_oc.sh",
+             shcode='R --no-save "--args ${SLURM_ARRAY_TASK_ID}" < profiling/1.simulation/1.B.2_phase_parent_sim2.R',
+             arrayjobs="1-10",
+             wd=NULL, jobid="pp-oc", email="yangjl0930@gmail.com")
+
+##>>> In this path: cd /home/jolyang/Documents/Github/phasing
+###>>> [ note: --ntasks=INT, number of cup ]
+###>>> [ note: --mem=16000, 16G memory ]
+###>>> RUN: sbatch -p serial --mem 8000 largedata/scripts/step4_sim_pp.sh
