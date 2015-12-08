933666eb14955119b9af69cf036d351fdfde151b
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-12-07 22:10:04 -0800

    set up bigmemm

--- profiling/2.cjdata/2.C.3_setup_pp.R
+++ profiling/2.cjdata/2.C.3_setup_pp.R
@@ -5,12 +5,12 @@
 source("~/Documents/Github/zmSNPtools/Rcodes/set_arrayjob.R")
 
 # run array job of impute_parents
-set_arrayjob(shid="largedata/scripts/step4_pp.sh",
+set_arrayjob(shid="largedata/scripts/step4_pp2.sh",
              shcode='R --no-save "--args ${SLURM_ARRAY_TASK_ID}" < profiling/2.cjdata/2.C.2_run_phase_parent.R',
-             arrayjobs="1-270",
+             arrayjobs="170-270",
              wd=NULL, jobid="imputeR-pp", email="yangjl0930@gmail.com")
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/phasing
 ###>>> [ note: --ntasks=INT, number of cup ]
 ###>>> [ note: --mem=16000, 16G memory ]
-###>>> RUN: sbatch -p serial slurm-scripts/step3_aj_ip.sh
+###>>> RUN: sbatch -p serial largedata/scripts/step4_pp.sh
