afc770d91dac51363045fce9c00ca7c27a315865
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-12-11 14:41:42 -0800

    organize results.

--- profiling/2.cjdata/2.B.2_setup_code.R
+++ profiling/2.cjdata/2.B.2_setup_code.R
@@ -5,12 +5,12 @@
 source("~/Documents/Github/zmSNPtools/Rcodes/set_arrayjob.R")
 
 # run array job of impute_parents
-set_arrayjob(shid="largedata/scripts/step2_ip3.sh",
+set_arrayjob(shid="largedata/scripts/test.sh",
              shcode='R --no-save "--args ${SLURM_ARRAY_TASK_ID}" < profiling/2.cjdata/2.B.1_code_impute_parent.R',
-             arrayjobs="401-670",
-             wd=NULL, jobid="ip400", email="yangjl0930@gmail.com")
+             arrayjobs="1-100",
+             wd=NULL, jobid="ip100", email="yangjl0930@gmail.com")
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/phasing
 ###>>> [ note: --ntasks=INT, number of cup ]
 ###>>> [ note: --mem=16000, 16G memory ]
-###>>> RUN: sbatch -p bigmemm largedata/scripts/step2_aj_ip.sh
+###>>> RUN: sbatch -p bigmemm largedata/scripts/step3_aj_ip.sh
--- profiling/2.cjdata/2.B.5_error_rate.R
+++ profiling/2.cjdata/2.B.5_error_rate.R
@@ -1,39 +1,77 @@
 ### Jinliang Yang
 ### 12/8/2015
 
-imp <- data.frame()
-for(i in 1:10){
-    tem <- read.csv(paste0("largedata/ip/chr", i, "_ip50.csv"))
-    imp <- rbind(imp, tem)
-}
-imp <- read.csv()
 
+library("data.table", lib="~/bin/Rlib")
+mx <- fread("largedata/lcache/teo_recoded.txt")
+mx <- as.data.frame(mx)
 
-snpinfo <- read.csv("largedata/obs/snpinfo.csv")
-snpinfo <- cbind(snpinfo, imp)
+imp <- fread("largedata/ip/imputed_parents.csv", sep=",")
+imp <- as.data.frame(imp)
 
 ped <- read.table("data/parentage_info.txt", header=TRUE)
+ped[, 1:3] <- apply(ped[, 1:3], 2, as.character)
+pinfo <- pedinfo(ped)
+pinfo <- subset(pinfo, nselfer > 30)
+pinfo$founder <- as.character(pinfo$founder)
 
-ob <- load(file="~/Documents/Github/imputeR/largedata/teo.RData")
-Geno4imputeR <- ob1
-geno <- Geno4imputeR@genomx
+############
+out <- imp_checking(imp, mx, ped, depth_cutoff=10)
+write.table(out, )
+
+imp_checking <- function(imp, mx, ped, depth_cutoff=10){
+    res <- pinfo
+    res$miss <- -9
+    res$gbs0 <- -9
+    res$gbs1 <- -9
+    res$gbs2 <- -9
+    res$call0 <- -9
+    res$call1 <- -9
+    res$call2 <- -9
+    res$err0 <- -9
+    res$err1 <- -9
+    res$err2 <- -9
+    res$err <- -9
+    for(i in 1:nrow(pinfo)){
+        focalp <- pinfo$founder[i]
+        kids <- subset(ped, parent1 == focalp & parent2 == focalp)$proid
+        kidgeno <- mx[, c("snpid", focalp, kids)]
+        message(sprintf("###>>> founder [ %s ] has [ %s ] selfed kids, [ %s ] selected!", 
+                        focalp, pinfo$nselfer[i], length(kids)))
+        
+        subgeno <- merge(snpinfo, kidgeno, by="snpid")
+        #sum(subgeno$major.x != subgeno$major.y)
+        pgeno <- imp[, c("snpid", focalp)]
+        names(pgeno)[2] <- "ip"
+        subgeno <- merge(pgeno, subgeno, by="snpid")
+        
+        subgeno$count0 <- apply(subgeno[, 11:ncol(subgeno)], 1, function(x) sum(x==0))
+        subgeno$count1 <- apply(subgeno[, 11:ncol(subgeno)], 1, function(x) sum(x==1))
+        subgeno$count2 <- apply(subgeno[, 11:ncol(subgeno)], 1, function(x) sum(x==2))
+        subgeno$count3 <- apply(subgeno[, 11:ncol(subgeno)], 1, function(x) sum(x==3))
+        
+        res$miss[i] <- sum(subgeno[,10] == 3)
+        res$gbs0[i] <- sum(subgeno[,10] == 0)
+        res$gbs1[i] <- sum(subgeno[,10] == 1)
+        res$gbs2[i] <- sum(subgeno[,10] == 2)
+        res$call0[i] <- sum(subgeno$ip == 0)
+        res$call1[i] <- sum(subgeno$ip == 1)
+        res$call2[i] <- sum(subgeno$ip == 2)
+        
+        t1 <- subset(subgeno,  count0 > depth_cutoff & count1 == 0 & count2 == 0)
+        res$err0[i] <- sum(t1$ip != 0)/length(t1$ip)
+
+        t2 <- subset(subgeno,  count0 == 0 & count1 == 0 & count2 > depth_cutoff)
+        res$err2[i] <- sum(t2$ip != 2)/length(t2$ip)
+        
+        t3 <- subset(subgeno,  count0 > depth_cutoff & count2 > depth_cutoff)
+        res$err1[i] <- sum(t3$ip != 1)/length(t3$ip)
+        
+        res$err[i] <- (sum(t1$ip != 0) + sum(t2$ip != 2) + sum(t3$ip != 1))/(length(t1$ip) + length(t2$ip) + length(t3$ip))
+    }
+    return(res)
+}
 
 
 
-focalp <- gsub(".*_PC", "PC", names(imp))
-focalp <- gsub("\\.", ":", focalp)
-subped <- subset(ped, parent1 ==  focalp[1] & parent2 == focalp[1])
-
-geno <- as.data.frame(geno)
-mysnp <- merge(snpinfo[, c(1:8, 9)], geno[, subped$proid], by.x="snpid", by.y="row.names")
-
-
-mysnp$count0 <- apply(mysnp[, 10:ncol(mysnp)], 1, function(x) sum(x==0))
-mysnp$count1 <- apply(mysnp[, 10:ncol(mysnp)], 1, function(x) sum(x==1))
-mysnp$count2 <- apply(mysnp[, 10:ncol(mysnp)], 1, function(x) sum(x==2))
-mysnp$count3 <- apply(mysnp[, 10:ncol(mysnp)], 1, function(x) sum(x==3))
-
-table(subset(mysnp, count1==0 & count2==0 & count3 <30)[, 9])
-
-table(subset(mysnp, count0==0 & count1==0 & count3 <30)[, 9])
 
