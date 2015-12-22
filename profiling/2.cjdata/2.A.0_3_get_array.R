87e6e9c972bf95c6a2f364fbb2101cc5aefe5600
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-12-22 12:01:01 -0800

    updated get_array.

--- profiling/2.cjdata/2.A.0_3_get_array.R
+++ profiling/2.cjdata/2.A.0_3_get_array.R
@@ -6,19 +6,29 @@ library(data.table, lib="~/bin/Rlib/")
 
 ped <- read.table("data/parentage_info.txt", header =TRUE)
 geno <- fread("largedata/lcache/teo_recoded.txt")
+geno <- as.data.frame(geno)
 
-mygeno <- mendelian_check(geno, ped, self_cutoff=30, depth_cutoff=10)
+
+snpinfo <- get_snpinfo(geno, ped, self_cutoff=30)
+write.table(snpinfo, "cache/snpinfo_self30.csv", sep=",", row.names=FALSE, quote=FALSE)
 
 
-ped[, 1:3] <- apply(ped[,1:3], 2, as.character)
+#################################################
+
 myped <- subset(ped, parent1 == parent2)
 tab <- table(myped$parent1)
 id <- names(tab[tab>30])
 myped <- subset(myped, parent1 %in% id)
 
+#mygeno <- mendelian_check(geno, ped, self_cutoff=30, depth_cutoff=10)
+pinfo <- pedinfo(ped)
+pinfo <- pinfo[order(pinfo$nselfer, decreasing = TRUE),]
 
-snpinfo <- create_array(geno=mygeno, ped=myped, outdir="largedata/obs/", snpinfo=NULL)
-write.table(snpinfo, "largedata/lcache/snpinfo.csv", sep=",", row.names=FALSE, quote=FALSE)
+ped[, 1:3] <- apply(ped[, 1:3], 2, as.character)
+myped <- subset(ped, parent1 == parent2 & parent1 == pinfo$founder[1])
+pargeno <- data.frame(parentid= as.character(unique(c(ped$parent1, ped$parent2))), true_p=0)
+
+create_array(geno, ped=myped, pargeno, outdir="largedata/obs", bychr=FALSE, snpinfo=snpinfo, self_cutoff=30)
 
 ###>>> Input [ 598043 ] biallelic loci for [ 4875 ] plants
 ###>>> Filtering loci with MAF < [ 0.002 ], Locus Missing Rate > [ 0.8 ] and Individual Missing Rate > [ 0.8 ]
