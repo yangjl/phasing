e04b96548e5f2c6506478a515b662eda72b3fe60
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-12-28 11:36:40 -0800

    updates

---
+++ profiling/2.cjdata/2.D.1_read_depth.sh
@@ -0,0 +1,5 @@
+# display depth information
+srun.x11 -p bigmemh --mem 64000 --ntasks=8 --nodelist=bigmem1
+module load gcc jdk/1.8 tassel/5
+
+run_pipeline.pl -Xmx64g -h5 RareAllelesC2Teo2015_ZeaGBSv27raw.h5 -export RareAllelesC2Teo2015_ZeaGBSv27raw.vcf -exportType VCF
