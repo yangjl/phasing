3ad334dfbb946ca7e4ce7564fd6f928c09326f3c
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-01-11 22:42:58 -0800

    gogogo!

--- profiling/4.cj_depth/4.C.4_collect_pp.R
+++ profiling/4.cj_depth/4.C.4_collect_pp.R
@@ -13,7 +13,14 @@ save(file="largedata/cjmasked/R1_pp24.RData", list="ppr1")
 source("lib/get_pp.R")
 ppr2 <- get_pp(path="largedata/cjmasked/obs2", pattern="PC_.*.csv", chunk_inc=100, imp68)
 #[1] 1130
+df <- list.files(path="largedata/cjmasked/obs2", pattern="PC_.*.RData", full.names = TRUE)
 files <- list.files(path="largedata/cjmasked/obs2", pattern="PC_.*.csv", full.names = TRUE)
+
+df <- gsub("RData", "csv", df)
+
+"largedata/cjmasked/obs2/PC_I05_ID1_1:250276198_chr4_chunk2.csv"
+idx <- df[!df %in% files]
+
 
 save(file="largedata/cjmasked/R2_pp21.RData", list="ppr1")
 
