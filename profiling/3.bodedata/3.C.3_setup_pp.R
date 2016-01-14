d2e75ed56639cfc212e6913e8120ae262f5ab00c
Jinliang Yang <yangjl0930@gmail.com>
2016-01-14 13:48:24 -0800

    running bode 2nd round pp

--- profiling/3.bodedata/3.C.3_setup_pp.R
+++ profiling/3.bodedata/3.C.3_setup_pp.R
@@ -21,16 +21,19 @@ set_arrayjob(shid="largedata/scripts/run_bode_pp1.sh",
 # pp round 2
 files <- list.files(path="largedata/bode/obs2", pattern="RData", full.names=TRUE)
 df <- data.frame(id=1:length(files), file=files)
+#1109
 write.table(df, "largedata/bode_pp_files.csv", sep=",", row.names=FALSE)
 
 #$SLURM_ARRAY_TASK_ID $SLURM_JOB_ID
 source("~/Documents/Github/zmSNPtools/Rcodes/set_arrayjob.R")
-set_arrayjob(shid="largedata/scripts/runbode_pp2.sh",
+set_arrayjob(shid="largedata/scripts/runbode_pp21.sh",
              shcode='R --no-save "--args ${SLURM_ARRAY_TASK_ID}" < profiling/3.bodedata/3.C.2_run_phase_parent.R',
-             arrayjobs="1-150",
-             wd=NULL, jobid="bode-ppr2", email="yangjl0930@gmail.com")
+             arrayjobs="1-800",
+             wd=NULL, jobid="pp21", email="yangjl0930@gmail.com")
+###>>> RUN: sbatch -p serial largedata/scripts/runbode_pp21.sh
 
-###>>> In this path: cd /home/jolyang/Documents/Github/phasing
-###>>> [ note: --ntasks=INT, number of cup ]
-###>>> [ note: --mem=16000, 16G memory ]
-###>>> RUN: sbatch -p bigmemm largedata/scripts/runbode_pp2.sh
+set_arrayjob(shid="largedata/scripts/runbode_pp22.sh",
+             shcode='R --no-save "--args ${SLURM_ARRAY_TASK_ID}" < profiling/3.bodedata/3.C.2_run_phase_parent.R',
+             arrayjobs="801-1109",
+             wd=NULL, jobid="pp22", email="yangjl0930@gmail.com")
+###>>> RUN: sbatch -p bigmemm largedata/scripts/runbode_pp22.sh
