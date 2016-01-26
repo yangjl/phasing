689034cc5dbff56c986fce6f32ae3b70b98121ed
Jinliang Yang <yangjl0930@gmail.com>
2016-01-25 20:01:32 -0800

    kids err

--- profiling/3.bodedata/3.D.4_ik_err_rate.R
+++ profiling/3.bodedata/3.D.4_ik_err_rate.R
@@ -28,7 +28,7 @@ write.table(kerr, "cache/landrace_post_kerr.csv", sep=",", row.names=FALSE, quot
 
 kerr <- read.csv("cache/landrace_post_kerr.csv")
 
-
+terr <- read.csv("cache/post_kerr.csv")
 
 
 
